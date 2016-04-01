7fe08f5a0b1838893a8787d6b60b9faf6f4c0867
SupeRodv2 <supgeek.rod@gmail.com>
2016-04-01 09:14:03 +0800

    fix timeline like

--- app/Http/Controllers/Api/TimelineController.php
+++ app/Http/Controllers/Api/TimelineController.php
@@ -133,7 +133,7 @@ class TimelineController extends Controller
             'id'        =>      'required',
         ]);
 
-        $TimelineLike = TimelineLike::where(['timeline_id' => $request->id])->first();
+        $TimelineLike = TimelineLike::where(['timeline_id' => $request->id, 'user_id' => Auth::user()->user()->id])->first();
         $Timeline = Timeline::findOrFail($request->id);
 
         if ($TimelineLike) {

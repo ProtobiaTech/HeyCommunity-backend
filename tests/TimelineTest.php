d8d0884a447dd0f33168e5fab99092e57ef7a2e8
SupeRodv2 <supgeek.rod@gmail.com>
2016-08-29 10:51:59 +0800

    restore tests dir

---
+++ tests/TimelineTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TimelineTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * test get index
+     *
+     * @return void
+     */
+    public function testGetIndex()
+    {
+        $this->get('/api/timeline/index')->seeJson(['is_like' => false]);
+    }
+
+    /**
+     * test store a new timeline
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $User = factory(App\User::class)->create();
+        $this->actingAs($User);
+
+
+        $data = [
+            'content'   =>      'this is content',
+        ];
+        $this->post('/api/timeline/store', $data)->assertResponseOk();
+    }
+}
---
+++ tests/UserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use Illuminate\Support\Facades\Auth;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * test user, signup login and logout
+     *
+     * @return void
+     */
+    public function testSignUpAndLogInAndLouOut()
+    {
+        $datas = [
+            [
+                'nickname'  =>  'nickname' . random_int(0, 1111),
+                'phone'     =>  123123 . random_int(0, 1111),
+                'password'  =>  'password',
+            ],
+            [
+                'nickname'  =>  'nickname2' . random_int(0, 1111),
+                'phone'     =>  '17012341234',
+                'password'  =>  'password',
+            ],
+        ];
+
+        foreach($datas as $data) {
+            $this->post('/api/user/sign-up', $data)->assertResponseOk();
+            $this->post('/api/user/log-out')->assertResponseOk();
+
+            $loginData = [
+                'phone'     =>  $data['phone'],
+                'password'  =>  $data['password'],
+            ];
+            $this->post('/api/user/log-in', $loginData)->assertResponseOk();
+            $this->post('/api/user/log-out')->assertResponseOk();
+        }
+    }
+}

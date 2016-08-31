3a921859e58a1ec74f2d31fed2d91d88ec5258ce
SupeRodv2 <supgeek.rod@gmail.com>
2016-08-31 16:00:42 +0800

    the notice model and softdelete

--- app/TimelineImg.php
+++ app/TimelineImg.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TimelineImg extends Model
 {
+    use SoftDeletes;
+
+    /**
+     *
+     */
     public static function getImgs($imgs)
     {
         $imgs = json_decode($imgs);

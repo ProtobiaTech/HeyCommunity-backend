18e548b54d49d5d3f9aeef6fd9ce8d610b8c10ec
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-13 12:18:58 +0800

    set db.config port

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'database'  => env('DB_DATABASE', 'heycommunity'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
+            'port'      => env('DB_PORT', '3306'),
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',

e65c18daf4e5eaab520a472a8d91f09ab8702f47
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-24 21:03:15 +0800

    the analytics and daoVoice

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -74,5 +74,44 @@
 
 
 @include('layouts.common')
+<div style="display:none">
+  <script src="https://s4.cnzz.com/z_stat.php?id=1260869881&web_id=1260869881" language="JavaScript"></script>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-68392745-10', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+</div>
+
+<!-- DaoVoice -->
+<script>(function(i,s,o,g,r,a,m){i["DaoVoiceObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;a.charset="utf-8";m.parentNode.insertBefore(a,m)})(window,document,"script",('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/86480a80.js","daovoice");</script>
+@if (Auth::admin()->guest())
+  <script>
+    daovoice('init', {
+      app_id: "86480a80",
+    });
+    daovoice('update');
+  </script>
+@else
+  <script>
+    daovoice('init', {
+      app_id: "86480a80",
+      private_deployment: "1",
+      domain: "{{ $_SERVER['HTTP_HOST'] }}",
+      ip: "{{ $_SERVER['SERVER_ADDR'] . ':' . $_SERVER['SERVER_PORT'] }}",
+      email: "{{ Auth::admin()->user()->email }}",
+      user_id: "{{ Auth::admin()->user()->id }}",
+      signed_up: {{ Auth::admin()->user()->created_at->getTimestamp() }},
+      name: "{{ Auth::admin()->user()->nickname }}",
+    });
+    daovoice('update');
+  </script>
+@endif
 </body>
 </html>

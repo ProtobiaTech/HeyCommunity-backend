39847def1d1d4d81e52d127c3178788e71fd41ad
supeRodv2 <supgeek.rod@gmail.com>
2017-02-25 14:29:25 +0800

    fix user seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,14 +15,14 @@ class UserTableSeeder extends Seeder
 
         \App\User::create([
             'nickname'      =>      'Rod',
-            'avatar'        =>      '/assets/images/userAvatar-default.png',
+            'avatar'        =>      'assets/images/userAvatar-default.png',
             'email'         =>      'supgeek.rod@gmail.com',
             'phone'         =>      '17090402884',
             'password'      =>      bcrypt('123123'),
         ]);
         \App\User::create([
             'nickname'      =>      'Test User',
-            'avatar'        =>      '/assets/images/userAvatar-default.png',
+            'avatar'        =>      'assets/images/userAvatar-default.png',
             'email'         =>      'test@hey-community.cn',
             'phone'         =>      '12312341234',
             'password'      =>      Hash::make('123123'),

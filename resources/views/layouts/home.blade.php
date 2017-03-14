1d5ab345d7db3fbafc970e386c2c04f7134c49cc
supeRodv2 <supgeek.rod@gmail.com>
2017-03-14 20:29:55 +0800

    fix home layout

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -49,7 +49,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ url('/topic') }}">Topic</a>

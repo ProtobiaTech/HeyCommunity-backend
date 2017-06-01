f49a4adf0fb11bb78a2cc85e74665c8014bdd2ce
ETY001 <ety001@domyself.me>
2017-06-01 19:44:25 +0800

    fix conflicts

--- app/Http/Controllers/Api/V1/TimelineController.php
+++ app/Http/Controllers/Api/V1/TimelineController.php
@@ -16,12 +16,13 @@ use App\TimelineVideo;
 use App\TimelineComment;
 use App\Notice;
 use App\Events\TriggerNoticeEvent;
-use Dingo\Api\Routing\Helpers;
+use App\Http\Controllers\Api\Transform\TimelineTransform;
 
-class TimelineController extends Controller
+/**
+ * @Resource("Timeline", uri="/timelines")
+ */
+class TimelineController extends BaseController
 {
-    use Helpers;
-
     /**
      * The construct
      *
@@ -29,11 +30,20 @@ class TimelineController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('api.auth', ['only'=>[]]);
+        $this->middleware('jwt.auth', ['only'=>[]]);
     }
 
     /**
-     * Get all of the resource.
+     * Show all timelines.
+     *
+     * Get a JSON representation of all the timelines.
+     *
+     * @Get("/")
+     * @Versions({"v1"})
+     * @Transaction({
+     *      @Request({"token": "foo"}),
+     *      @Response(200, body={"data": []}),
+     * })
      *
      * @param \Illuminate\Http\Request $request
      * @return object The all timelines
@@ -53,31 +63,48 @@ class TimelineController extends Controller
 
         $user = $this->auth->user();
 
-        $timelines = $query->get()->each(function($item, $key)use($user){
+        $timelines = $query->get()->each(function($item, $key) use ($user) {
             if (!$user) {
                 $item->is_like = false;
             } else {
-                $item->is_like = TimelineLike::where(['timeline_id' => $item->id, 'user_id' => $user->id])->count() ? true : false;
+                $item->is_like = TimelineLike::where([
+                                    'timeline_id' => $item->id,
+                                    'user_id' => $user->id
+                                ])->exists() ? true : false;
             }
             if ($item->imgs) {
                 $item->imgs = TimelineImg::getImgs($item->imgs);
             }
-        })->toArray();
+        });
 
-        return $timelines;
+        return $this->response
+                    ->collection($timelines, new TimelineTransform)
+                    ->setStatusCode(200);
     }
 
     /**
      * Get one of the resource according $id.
      *
+     * @Get("/{id}")
+     * @Versions({"v1"})
+     * @Transaction({
+     *      @Response(200, body={"data": {}}),
+     *      @Response(404, body={"message":"Not Found","status_code":404})
+     * })
      * @param \Illuminate\Http\Request $request
      * @param int $id
      * @return object The timeline
      */
-    public function detail(Request $request, $id)
+    public function show(Request $request, $id)
     {
         $timeline = Timeline::where('id', $id)
                     ->first();
-        return $timeline;
+        if ($timeline) {
+            return $this->response
+                        ->item($timeline, new TimelineTransform)
+                        ->setStatusCode(200);
+        } else {
+            return $this->errorNotFound();
+        }
     }
 }
